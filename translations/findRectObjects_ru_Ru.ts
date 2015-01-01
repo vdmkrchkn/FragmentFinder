b9e1204ad03c5c75f3080673ad60a86af1f0d180
vdmkrchkn <vdmkrchkn@gmail.com>
2015-01-01 22:50:33 +0300

    Добавлен файл переводов

---
+++ translations/findRectObjects_ru_Ru.ts
@@ -0,0 +1,100 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Image viewer</source>
+        <translation>Просмотр изображений</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="28"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="36"/>
+        <source>&amp;Image</source>
+        <translation>&amp;Изображение</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="46"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Открыть</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="49"/>
+        <source>Open an image</source>
+        <translation>Открыть изображение</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="52"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="57"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Выход</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="60"/>
+        <source>Close the application</source>
+        <translation>Закрыть данное приложение</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="63"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="68"/>
+        <source>Find F&amp;ragment</source>
+        <translation>&amp;Найти фрагмент</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="71"/>
+        <source>Find fragment on current image</source>
+        <translation>Найти фрагмент на данном изображении</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="74"/>
+        <source>Ctrl+R</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="82"/>
+        <location filename="../mainwindow.cpp" line="95"/>
+        <source>Open File</source>
+        <translation>Открыть изображение</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="84"/>
+        <location filename="../mainwindow.cpp" line="97"/>
+        <source>Images (*.png *.jpg)</source>
+        <translation>Файлы изображений (*.png *.jpg)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="92"/>
+        <location filename="../mainwindow.cpp" line="101"/>
+        <source>Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="92"/>
+        <source>No image to process</source>
+        <translation>Нет изображения для обработки</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="101"/>
+        <source>No pattern image is loaded</source>
+        <translation>Не загружен шаблон изображения</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="148"/>
+        <source>File loaded</source>
+        <translation>Файл загружен</translation>
+    </message>
+</context>
+</TS>
